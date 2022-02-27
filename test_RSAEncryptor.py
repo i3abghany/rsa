806f1ce85f080f2d28e635a64344ae66256839bb
Mahmoud Mandour <Mahmoudabdalghany@outlook.com>
2022-02-27 16:14:37 +0200

    Add simple encryption/decryption tests.

---
+++ test_RSAEncryptor.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+import Encode
+from RSAEncryptor import RSAEncryptor
+
+
+class TestRSAEncryptor(TestCase):
+
+    def test_encrypt_small_message(self):
+        m = 'Seat thyself sultanically among the moons of Saturn'
+        rsa = RSAEncryptor()
+        enc = rsa.encrypt(Encode.to_numeric(m))
+        dec = Encode.to_string(rsa.decrypt(enc))
+        self.assertEqual(m, dec)
+
+    def test_numeric_message(self):
+        m = 656112132172731831939213281327132
+        rsa = RSAEncryptor()
+        enc = rsa.encrypt(m)
+        dec = rsa.decrypt(enc)
+        self.assertEqual(m, dec)
