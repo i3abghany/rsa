99a6d3d7a3e192a68113fa673b25bd21342eb712
Mahmoud Mandour <Mahmoudabdalghany@outlook.com>
2022-02-27 15:39:51 +0200

    Initial commit, add RSAEncryptor.

---
+++ RSAEncryptor.py
@@ -0,0 +1,24 @@
+
+from Cryptodome.Util import number
+
+
+class RSAEncryptor:
+    DEFAULT_PUBLIC_EXPONENT = 65537
+
+    def __init__(self, prime_length=1024):
+        self.prime_length = prime_length
+        self._gen_keys()
+
+    def _gen_keys(self):
+        self._p = number.getPrime(self.prime_length)
+        self._q = number.getPrime(self.prime_length)
+        self._mod = (self._p - 1) * (self._q - 1)
+        self._n = self._p * self._q
+        self._e = self.DEFAULT_PUBLIC_EXPONENT
+        self._d = pow(self._e, -1, self._mod)
+
+    def encrypt(self, m: int):
+        return pow(m, self._e, self._n)
+
+    def decrypt(self, m_prime: int):
+        return pow(m_prime, self._d, self._n)
